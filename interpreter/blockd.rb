fe0afa96c1cd270e8ddac96d1955dd5ad42dd1c3
Christopher Bertels <bakkdoor@flasht.de>
2009-05-27 00:40:08 +0200

    fixed the grammar to correctly parse comments. also updated the blockd.rb script to use the treetop-grammar parser. enhanced the test.blk sample file.

--- interpreter/blockd.rb
+++ interpreter/blockd.rb
@@ -1,17 +1,22 @@
 #!/usr/bin/env ruby
 
+require "rubygems"
+require "treetop"
+
 # expand search path correct subdir.
 $: << File.expand_path(File.dirname(__FILE__))
 
 require "lib/core"
-require "parser"
 require "pp"
+#require "grammar/blockd.tt" # this will get loaded with treetop to generate the BlockdParser
+
+Treetop.load "grammar/blockd.tt"
 
 if ARGV.size < 1
   raise "Error: Please specify a source file to interpret."
 end
 
 parse_file = ARGV[0]
-#parser = Parser.new(parse_file)
-ast = Parser.new(parse_file).parse
+parser = BlockdParser.new
+ast = parser.parse IO.read(parse_file)
 PP.pp ast

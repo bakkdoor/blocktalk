396b8b010940d10ec5b8010df136bfd297dc5a83
Christopher Bertels <bakkdoor@flasht.de>
2009-05-29 01:36:28 +0200

    fixed bug that caused method calls to not parse & eval correctly.

--- interpreter/parser/nodes/message_with_params.rb
+++ interpreter/parser/nodes/message_with_params.rb
@@ -2,7 +2,7 @@ module Blockd
   class MessageWithParamsNode < Treetop::Runtime::SyntaxNode
     def value
       { :message => message.value,
-        :params => params}
+        :params => params.value}
     end
   end
 end

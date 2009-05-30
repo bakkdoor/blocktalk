0a34a2ef875e3bd8b700e67166e2a12454df89fb
Christopher Bertels <bakkdoor@flasht.de>
2009-05-31 01:14:59 +0200

    added Class#>> as a shorthand to Class#in .. Updated example file and some other minor changes

--- interpreter/lib/kernel/class.rb
+++ interpreter/lib/kernel/class.rb
@@ -8,12 +8,25 @@ class Class
         Kernel::const_set(class_name_sym, Class.new(superclass_name_sym, &block))
       end
     else
-      if classobj = Kernel::const_get(class_name_sym)
+      if Kernel::constants.include?(class_name_sym)
+        classobj = Kernel::const_get(class_name_sym)
         classobj.class_eval(&block)
       else
         Kernel::const_set(class_name_sym, Class.new(&block))
       end
     end
+  end
+
+  def self.>>(class_name_sym, &block)
+    class_name = class_name_sym
+    superclass_name = nil
+
+    if class_name_sym.is_a?(Hash)
+      class_name = class_name_sym.keys.first
+      superclass_name = class_name_sym.values.first
+    end
+
+    self.in(class_name, superclass_name, &block)
   end
 
   def extend(&block)

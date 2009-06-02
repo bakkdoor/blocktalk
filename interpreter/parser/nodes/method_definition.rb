b435349a4cfa8ef829d41c1ade9072c894b9f259
Christopher Bertels <bakkdoor@flasht.de>
2009-06-02 15:24:51 +0200

    when defining methods without params, simply pass an param-less imlicit block to define_method instead of creating a codeblock object first.

--- interpreter/parser/nodes/method_definition.rb
+++ interpreter/parser/nodes/method_definition.rb
@@ -1,7 +1,12 @@
 module Blockd
   class MethodDefinitionNode < Treetop::Runtime::SyntaxNode
     def value
-      "define_method(:#{method_name.value}, &#{method_body.value})"
+      if method_body.params.respond_to?(:value)
+        "define_method(:#{method_name.value}, &#{method_body.value})"
+      else
+        # no parameters given -> simply call define_method with a param-less block
+        "define_method(:#{method_name.value}){#{method_body.body.value}}"
+      end
     end
   end
 end

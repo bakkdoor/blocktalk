ecbb36ea0c48352d2ad433a5782f91a83140222a
Christopher Bertels <bakkdoor@flasht.de>
2009-06-02 15:19:00 +0200

    small optimization when passing implicit blocks to methodcalls (avoid creating a codeblock object, that then gets turned into a block right away)

--- interpreter/parser/nodes/methodcall.rb
+++ interpreter/parser/nodes/methodcall.rb
@@ -9,10 +9,19 @@ module Blockd
       eval_str += "#{params.join(', ')}"
 
       if passed_block.class == Blockd::BlockLiteralNode
-        if params.size > 0
-          eval_str += ", "
+        eval_str += "){" # start block
+
+        # check for block_params
+        if passed_block.params.respond_to?(:value)
+          eval_str += "|"
+          eval_str +=
+            passed_block.params.value.collect{|p| p[:identifier]}.join(",")
+          eval_str += "| "
         end
-        eval_str += "&" + passed_block.value
+
+        eval_str += passed_block.body.value # insert block-body
+        eval_str += "}" # end block
+
       elsif passed_block.text_value =~ /&\S+/
         # check for Proc-Objects passed to method as block
         if params.size > 0
@@ -20,9 +29,11 @@ module Blockd
         end
         eval_str += "&"
         eval_str += passed_block.block_var_name.value
+        eval_str += ")"
+      else
+        # no block given -> method call is finished
+        eval_str += ")"
       end
-
-      eval_str += ")"
 
       return eval_str
     end

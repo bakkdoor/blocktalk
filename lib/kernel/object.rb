fdd3838c5740385cfdebbc0df2c7778eb4323451
Christopher Bertels <bakkdoor@flasht.de>
2009-06-05 20:46:16 +0200

    added support to call ruby methods with multiple arguments. for now it works this way: if a method is called, that isn't defined in a ruby class, ruby falls back to PObject#method_missing (defined in lib/kernel/object). there, it'll look, if it's a blocktalk-typical methodname (with a double underscore: "__") and if so, creates the method as a cache (to not rely on the slow method_missing again) and finally calls the correct ruby method with the given arguments.

--- lib/kernel/object.rb
+++ lib/kernel/object.rb
@@ -30,4 +30,26 @@ class Object
       block.call
     end
   end
+
+  # this should get called, if we try to call a method on objects of
+  # ruby classes.
+  # it will try to find the correct ruby method-name & clall it.
+  def method_missing(name, *args, &block)
+    # probably a blocktalk_like method was called, when we actually
+    # ment a ruby method
+    splitted = name.to_s.split("__")
+    if splitted.size == 2
+      ruby_name = splitted.first
+
+      # hopefully, we got it right
+      # to make us not do the work again simply create the method for
+      # later use and then call it
+      arg_names = 0.upto(args.size - 1).collect{|i| "arg_#{i}"}
+      self.instance_eval "def #{name}(#{arg_names.join(',')}, &block); self.send(:#{ruby_name}, #{arg_names.join(',')}, &block); end;"
+      # now call!
+      self.send(ruby_name, *args, &block)
+    else
+      raise "Unknown method, don't know what to do: #{self.class}##{name}"
+     end
+  end
 end

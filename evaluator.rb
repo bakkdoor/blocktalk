c614a894a92eca2baf7d982e977064e7010b92b6
Christopher Bertels <bakkdoor@flasht.de>
2009-06-05 14:38:39 +0200

    changed evaluator to always have core-lib required

--- evaluator.rb
+++ evaluator.rb
@@ -1,5 +1,7 @@
 class Evaluator
-  @expressions = []
+  @load_path = File.dirname(__FILE__)
+  @expressions = ["require \"#{@load_path}/lib/core\""]
+
   def self.add(code_str)
     unless code_str.nil? or code_str.empty?
       @expressions << code_str
@@ -7,9 +9,8 @@ class Evaluator
   end
 
   def self.eval(argv = [])
-    load_path = File.dirname(__FILE__)
 #    Kernel::eval @expressions.join(";")
-    system("/usr/bin/env ruby -e 'require \"#{load_path}/lib/core\";#{@expressions.join(';')}' #{argv.join(' ')}")
+    system("/usr/bin/env ruby -e '#{@expressions.join(';')}' #{argv.join(' ')}")
   end
 
   def self.inspect

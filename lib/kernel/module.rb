891c6a7fd35b0f14f46da8b97266b99ae1a149b7
Christopher Bertels <bakkdoor@flasht.de>
2009-06-15 17:48:51 +0200

    nested modules are now supported. (yay!)

--- lib/kernel/module.rb
+++ lib/kernel/module.rb
@@ -1,11 +1,21 @@
 class Module
+  @@mod_stack = []
   def self.in(module_name_sym, &block)
-    if Kernel::constants.include?(module_name_sym.to_s)
-      moduleobj = Kernel::const_get(module_name_sym)
-      moduleobj.module_eval(&block)
-    else
-      Kernel::const_set(module_name_sym, Module.new(&block))
-    end
+    parent_mod = (["Kernel"] + @@mod_stack).join('::')
+
+    str = <<-REND
+        unless #{parent_mod}.const_defined?('#{module_name_sym}')
+          #{parent_mod}.module_eval '#{module_name_sym} = Module.new'
+        end
+    REND
+    Kernel.module_eval str
+
+    @@mod_stack.push module_name_sym
+
+    mod = Kernel.module_eval("#{@@mod_stack.join('::')}")
+    mod.module_eval(&block)
+
+    @@mod_stack.pop
   end
 
   def self.>>(module_name_sym, &block)
